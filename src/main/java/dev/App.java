320893bc8407feb6dd603d8fee530294b1e5712d
Asia Trabelsi <assia.trabelsi93@gmail.com>
2017-11-23 15:20:44 +0100

    3em commit

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,5 +1,7 @@
 package dev;
 
+import java.util.ResourceBundle;
+
 import com.github.lalyos.jfiglet.FigletFont;
 
 /**
@@ -10,8 +12,16 @@ public class App
 {
     public static void main( String[] args )
     {
-    	String asciiArt = FigletFont.convertOneLine("hello");
-        System.out.println(asciiArt);
+    	//String asciiArt = FigletFont.convertOneLine("HELLO WORLD");
+    	String titre = ResourceBundle.getBundle("application").getString("titre"); 
+    	String asciiArt = FigletFont.convertOneLine(titre);
+    	System.out.println(asciiArt);
+    	
       
+    	
+    	// affichage de l'environnement
+    	String environnement = ResourceBundle.getBundle("application").getString(
+    	"environnement");
+    	System.out.println("Environnement : " + environnement);
     }
 }
